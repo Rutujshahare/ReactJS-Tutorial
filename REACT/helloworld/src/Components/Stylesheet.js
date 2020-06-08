d5069196c87b2688f1e1f49e7c4bbd05e21592c9
Rahul Shahare <rahulshahare@gmail.com>
2020-06-08 14:46:20 +0530

    Keep the Code Clean
    Keep the code Clean and more readable

--- REACT/helloworld/src/Components/Stylesheet.js
+++ REACT/helloworld/src/Components/Stylesheet.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react'
 
- class Stylesheet extends Component {
-     constructor(props)
-     {
-         super(props)
-         this.state=
-         {
+class Stylesheet extends Component{
+     constructor(props){
+         super(props);
+         this.state = {
              message:'than you for the register'
          }
      }
-     click()
-     {
-         this.setState(
-             {
+     click(){
+         this.setState({
                  message:'welcome to the Gorakshan App'
-             }
-         )
-
+             })
      }
      
     render() {
         return (
             <div className='Heading'>
                <h2>{this.state.message}</h2> 
-        <div className="button" onClick={this.click.bind(this)}><button>Get Started</button></div>
+               <div>
+                    <button className="button" onClick={()=>this.click}>Get Started</button>
+               </div>
             </div>
             
         )
