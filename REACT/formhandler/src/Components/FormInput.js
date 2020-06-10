848fe8c61c994bc29d0aacd1b8a1006404a16dac
Rutujshahare <51992103+Rutujshahare@users.noreply.github.com>
2020-06-10 16:34:05 +0530

    data Map

--- REACT/formhandler/src/Components/FormInput.js
+++ REACT/formhandler/src/Components/FormInput.js
@@ -3,7 +3,26 @@ import Input from './Input';
 
 export default class FormInput extends React.Component{
     constructor(){
-        super(propd);
+        super();
+        this.state = {
+            data : [
+                    {
+                      "id": 1,
+                      "name": "Leanne Graham",
+                      "username": "Bret",
+                      "email": "Sincere@april.biz",
+                      "phone": "1-770-736-8031 x56442",
+                      "website": "hildegard.org",
+                    },
+                    {
+                        "id": 2,
+                        "name": "Leanne Graham",
+                        "username": "Bret",
+                        "email": "Sincere@april.biz",
+                        "phone": "1-770-736-8031 x56442",
+                        "website": "hildegard.org",
+                      }]
+        }
         
         this.componentRef=React.createRef();
 
@@ -11,7 +30,8 @@ export default class FormInput extends React.Component{
         
     }
     clickhandler(){
-        this.componentRef.current.focus()
+        // this.componentRef.current.focus()
+        console.log(this.state.data)
     }
     // clickhandler = () =>{
     //     this.componentRef.current.focus()
@@ -21,8 +41,25 @@ export default class FormInput extends React.Component{
             <div>
              <Input  ref={this.componentRef}/>
              <button onClick={this.clickhandler} >FormInput</button>
+             <div>
+                 {<MadData data={this.state.data}/>}
+             </div>
             </div>
         )
     }
+}
+
+function MadData(props){
+    
+    return(
+       
+       props.data.map(d =>{
+           console.log(d)
+           console.log(d.name)
+           console.log(d.phone)
+           console.log(d.username)
+       })
+     
+    )
 }
 // export default FormInput
